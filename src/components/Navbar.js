172aa2170ac4d5697446f9581a1ca60e0bea0cbd
Paulmak21 <paulgrozescu@gmail.com>
2023-10-31 09:41:47 +0100

    Merge branch 'main' of https://github.com/Paulmak21/landing-page-react

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../index.css';
+
+
+ function Navbar() {
+  return (<>
+
+    <div>
+      <nav className="fixed z-100 space-x-20 m-5 text-white">
+        <img src='/images/bmw-car-logo-luxury-vehicle-bmw-lovo.jpg' alt='BMW Logo'></img>
+        <button>Home</button>
+        <button>Automotive Life</button>
+        <button>Design</button>
+        <button>Innovation</button>
+        <button>Performance</button>
+        <button>Modèles</button>
+            
+      </nav>
+      <div className='text-white'>ASdasd</div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Navbar;
